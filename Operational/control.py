2481947c8ea710279057a7441cc860acc181714c
Andres Ladino <aladinoster@gmail.com>
2018-11-19 17:16:48 +0100

    Control classes

---
+++ Operational/control.py
@@ -0,0 +1,21 @@
+"""
+    This file defines the classes and objects regarding the control strategy 
+    
+"""
+
+
+from parameters import SimParameter, CtrParameter
+
+
+class TacticalCtrl(SimParameter):
+    """
+    Tactical Control Class
+    """
+    pass
+
+
+class OperationalCtr(CtrParameter, SimParameter):
+    """
+    Operational Control Class 
+    """
+    pass
