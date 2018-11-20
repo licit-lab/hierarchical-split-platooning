59c3928fc410ce116b7ed7902772507132721677
Andres Ladino <aladinoster@gmail.com>
2018-11-20 07:18:10 +0100

    Added reference computation

--- Operational/control.py
+++ Operational/control.py
@@ -13,6 +13,15 @@ class TacticalCtrl(SimParameter):
     """
     pass
 
+        for veh_id, event in zip(self):
+            tau0 = event.get('tau0', 1)
+            tauf = event.get('tauf', 2)
+            tmerge = event.get('tm', 2)
+            tantcp = event.get('ta', 1)
+            b = tmerge - tantcp / 2  # Phase
+            K = 8 * tantcp  # Compression
+            r = tau0 + (tauf-tau0) / (1 + np.exp(-K*(t-b))
+            self.reference[veh_id]=r
 
 class OperationalCtr(CtrParameter, SimParameter):
     """
