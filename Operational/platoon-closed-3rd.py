e8661e42cc321e37e924b6491f4201611b708a70
Andres Ladino <aladinoster@gmail.com>
2018-11-20 18:38:40 +0100

    Add leaders + Tactical /Op Control initializers

--- Operational/platoon-closed-3rd.py
+++ Operational/platoon-closed-3rd.py
@@ -13,11 +13,10 @@
     Usage:
     python platoon-closed-3rd.py
 """
-from parameters import VehParameter, SimParameter
+from parameters import VehParameter, SimParameter, CtrParameter
 from models import VehNetwork, Vehicle, dynamic_3rd
 from control import OperationalCtr, TacticalCtrl
 
-from matplotlib import pyplot as plt
 
 # Length of the platoon
 N_VEH = 8
@@ -40,11 +39,20 @@ S0 = 10.0
 veh_par = VehParameter.VehParameterSym(U_FFS, K_X, W_CGT, L_VEH)
 
 # Create list of vehicles
-veh_list = [Vehicle(sim_par, veh_par, dynamic_3rd, id=i) for i in range(N_VEH)]
+list_id = range(N_VEH)
+veh_list = [Vehicle(sim_par, veh_par, dynamic_3rd, id=i) for i in list_id]
+
+# Artificial leaders:
+# Created a-priori
+list_id = list(list_id)[1:]
+lead_id = [i-1 for i in list_id[0:-1]]
+net_veh = dict(zip(list_id, lead_id))
 
 # Create the network of vehicles
-veh_network = VehNetwork(sim_par, veh_list)
-print(vars(veh_network))
+platoon = VehNetwork(sim_par, veh_list)
+print(f'Vehicle network: {net_veh}')
+platoon.register_vehicle_link(net_veh)
+
 
 # Initialize the network of vehicles
 x0 = [0.0] * N_VEH
@@ -56,20 +64,19 @@ a0 = [0.0] * N_VEH  # only 3rd order models
 # Initialize each vehicle
 state0 = [s0, v0, e0, a0]
 state0veh = list(zip(*state0))  # state each vehicle
-state0net = dict(zip(veh_network.veh_currentids, state0veh))
-veh_network.initialize_vehicles(state0net)
+state0net = dict(zip(platoon.veh_currentids, state0veh))
+platoon.initialize_vehicles(state0net)
 
 # Create the controller[]
-op_ctrl = OperationalCtr()
-event = {1: {'ta': 1, 'tm': 2, 'tau0': 1, 'tauf': 2}}
-tc_ctrl = TacticalCtrl(veh_network.sim_par, event)
-for k, v in tc_ctrl:
-    print(k, v)
-    #
+ctr_par = CtrParameter()
+
+split_events = {1: {'ta': 20, 'tm': 40, 'tau0': 2, 'tauf': 5}, }
+tc_ctrl = TacticalCtrl(platoon.sim_par, ctr_par, split_events)
 tc_ctrl.compute_reference()
 
-# print(len(sim_par.get_simtime_vector()))
-# tc_ctrl.compute_reference()
+op_ctrl = OperationalCtr(platoon.sim_par, ctr_par)
+op_ctrl.register_veh_network(platoon)
+op_ctrl.build_global_dynamics()
 
 # veh_network.launch_simulation()
 
